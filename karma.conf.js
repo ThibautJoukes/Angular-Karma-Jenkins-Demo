c13945cb95937c4306cb626d9e5160e553dda563
Thibaut <Joukes>
2019-12-31 12:07:49 +0100

    edit config file

--- karma.conf.js
+++ karma.conf.js
@@ -20,12 +20,12 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters = ['dots', 'junit'];
+    reporters: ['dots', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
     singleRun: true,
     restartOnFileChange: true,
     junitReporter: {
